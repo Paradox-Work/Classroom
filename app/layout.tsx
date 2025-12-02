26214876f31335d71ecf250ed330a83c392bdca4
Paradox-Work <ipa23.a.vasiljevs@vtdt.edu.lv>
2025-12-02 22:54:30 +0200

    npx prisma studio is a great f command yk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 
 
 const geistSans = Geist({
@@ -29,7 +30,8 @@ export default function RootLayout({
        <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
-      >
+    >
+      <ToastProvider />
         {children}
       </body>
     </html>
